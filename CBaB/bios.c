87c8ecc36bb02013cfa4a8e82859bfc0d9881e3b
Erik <122167559+EriktheRDev@users.noreply.github.com>
2023-02-18 10:41:39 -0500

    Update bios.c

--- CBaB/bios.c
+++ CBaB/bios.c
@@ -11,15 +11,33 @@ int main() {
   sleep(3); //Waits 3 Seconds.
   printf("\e[1;1H\e[2J"); //Clears Screen.
   int betabuild = -1; //Null
-  if (ver < 100) { //Checks to see if the build is in Beta {Less than 100}
-    betabuild = 1; //True
+  switch (ver) { //Simpler Version Management
+    case 100: //If the Version is 100,
+      betabuild = 0; //Set Betabuild to False
+      printf("%s", os);
+      break; //Exits
+    default: //If Version is NOT 100,
+      betabuild = 1; //Set Betabuild to True
+      printf("%s", os); //Prints OS
+      printf("[!] The Current Version of Cobalt is in beta. If available, please upgrade to a newer version. [!]\n"); //Prints the beta disclaimer
+      break; //Exits
   }
-  else { //If this isn't in beta
-    betabuild = 0; //False
+  sleep(3);
+  const betastatus = betabuild;
+  switch (betastatus) { //Checks to see if the previous check worked properly, and checks if the variable 'BETABUILD' is 0 or 1, and not -1
+    case -1: //If BetaBuild is -1 (Should not be -1, if it is prints an Error.)
+      printf("\e[1;1H\e[2J"); //Clears SCreen
+      printf("%s", os); //Prints OS Name
+      char errortype[5] = "A.A"; //Creates the Error Type
+      printf("The OS has Experienced a fatal error which has caused it to crash.\nTry Restarting. If the Error Persists, post this issue on Github.");
+      printf("\nSTOPCODE: A.A {BETABUILD_NOT_POSITIVE_OR_ZERO}\n"); //Crashes, Gives information on Crash
+      return 1;
   }
-  printf("COBALT\n"); //Prints Cobalt once again
-  if (betabuild = 1) { //Checks to see if the current build is in Beta
-    printf("The Current Version of Cobalt that you are using is in Beta. If available, please upgrade to a newer version.\n");
-    sleep(3);
+  printf("\n>> Betabuild has been changed from -1 [Null] to ");
+  switch (betastatus) {
+    case 0:
+      printf("0. {FALSE}\n");
+    case 1:
+      printf("1. {TRUE}\n");
   }
 }
