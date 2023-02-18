b3519bd4db94510259f326be8d160b7f12a341a3
Arnav Thorat <arnavthorat78@gmail.com>
2023-02-19 09:44:52 +1100

    Added a few extra commands (e.g. `cwd`)
    Co-authored-by: SturdyBen <benslife12@users.noreply.github.com>
    Co-authored-by: Erik <EriktheRDev@users.noreply.github.com>

--- CBaB/bios.c
+++ CBaB/bios.c
@@ -1,47 +1,68 @@
 #include <stdio.h>
 #include "services.h"
+
+//colors for text
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define BLU "\x1B[34m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define WHT "\x1B[37m"
+#define BOLD "\e[1m"
+#define RESET "\x1B[0m"
+
 int bios() {
-  printf("\e[1;1H\e[2J"); //Clears Screen.
-  char os[10] = "COBALT\n"; //Opearting System Name, Maximum of 10 characters.
-  int ver = 4; //Operating System Build, Maximum of 5 characters.
-  printf("%s", os); //Prints OS Name
-  printf("%s", "BUILD: "); //Prints Build
-  fflush(stdout); //Flushes
-  printf("%i", ver); //Prints Version directly after Build
-  fflush(stdout); //Flushes
-  sleep(3); //Waits 3 Seconds.
-  printf("\e[1;1H\e[2J"); //Clears Screen.
-  int betabuild = -1; //Null
-  switch (ver) { //Simpler Version Management
-    case 100: //If the Version is 100,
-      betabuild = 0; //Set Betabuild to False
-      printf("%s", os);
-      break; //Exits
-    default: //If Version is NOT 100,
-      betabuild = 1; //Set Betabuild to True
-      printf("%s", os); //Prints OS
-      printf("[!] The Current Version of Cobalt is in beta. If available, please upgrade to a newer version. [!]\n"); //Prints the beta disclaimer
-      break; //Exits
+  int delay = 1; // The time to wait on the sleep() functions
+  printf("\e[1;1H\e[2J");             // Clears Screen.
+  char os[25] = BLU "COBALT\n" RESET; // Opearting System Name, Maximum of 10 characters.
+  int ver = 4;                        // Operating System Build, Maximum of 5 characters.
+
+  printf("%s", os);        // Prints OS Name
+  printf("%s", "BUILD: "); // Prints Build
+  fflush(stdout);          // Flushes
+  printf("%i", ver);       // Prints Version directly after Build
+  fflush(stdout);          // Flushes
+
+  sleep(delay); // Waits 3 Seconds.
+
+  printf("\e[1;1H\e[2J"); // Clears Screen.
+  int betabuild = -1;     // Null
+  switch (ver) {                // Simpler Version Management
+  case 100:        // If the Version is 100,
+    betabuild = 0; // Set Betabuild to False
+    printf("%s", os);
+    break;                                                                                                                                  // Exits
+  default:                                                                                                                                  // If Version is NOT 100,
+    betabuild = 1;                                                                                                                          // Set Betabuild to True
+    printf("%s", os);                                                                                                                       // Prints OS
+    printf(YEL  "[!] You are currently running a beta build of Cobalt. Please upgrade to a newer version as soon as possible. [!]\n" RESET); // Prints the beta disclaimer
+    break;                                                                                                                                  // Exits
   }
-  sleep(3);
+
+  sleep(delay);
   const betastatus = betabuild;
-  switch (betastatus) { //Checks to see if the previous check worked properly, and checks if the variable 'BETABUILD' is 0 or 1, and not -1
-    case -1: //If BetaBuild is -1 (Should not be -1, if it is prints an Error.)
-      printf("\e[1;1H\e[2J"); //Clears SCreen
-      printf("%s", os); //Prints OS Name
-      char errortype[5] = "A.A"; //Creates the Error Type
-      printf("The OS has Experienced a fatal error which has caused it to crash.\nTry Restarting. If the Error Persists, post this issue on Github.");
-      printf("\nSTOPCODE: A.A {BETABUILD_NOT_POSITIVE_OR_ZERO}\n"); //Crashes, Gives information on Crash
-      return 1;
-  }
+  switch (betastatus) { // Checks to see if the previous check worked properly, and checks if the variable 'BETABUILD' is 0 or 1, and not -1
+  case -1:                     // If BetaBuild is -1 (Should not be -1, if it is prints an Error.)
+    printf("\e[1;1H\e[2J");    // Clears SCreen
+    printf("%s", os);          // Prints OS Name
+    char errortype[5] = "A.A"; // Creates the Error Type
+    printf(RED "A fatal error has occurred which has caused Cobalt to crash.\nCopy down the error code and then try restarting. If the problem persists, try contacting the developers of Cobalt.");
+    printf(RED "\nSTOP: A.A [INVALID_BETA_STATUS]\n" RESET); // Crashes, Gives information on Crash
+    return 1;
+  } // I'm gonna release Version 4 [The Previous Code] that I made.
+
   printf("\n>> Betabuild has been changed from -1 [Null] to ");
+
+  // controls weather the user is in a beta version or not(?)
   switch (betastatus) {
-    case 0:
-      printf("0 [False].\n");
-    case 1:
-      printf("1 [True].\n");
+  case 0:
+    printf("0 [False].\n");
+  case 1:
+    printf("1 [True].\n");
   }
+
   printf("\n>> Starting Services... ");
-  sleep(3);
+  sleep(delay);
   services();
 }
--- CBaB/services.c
+++ CBaB/services.c
@@ -1,21 +1,76 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
+//colors for text
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define BLU "\x1B[34m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define WHT "\x1B[37m"
+#define BOLD "\e[1m"
+#define RESET "\x1B[0m"
+
 int services() {
-  //Here's where all the Application Functions will be stored. Kind of Lonely here right now.
+  // Here's where all the Application Functions will be stored. Kind of Lonely here right now.
   printf("\n>> Complete.\n");
-  int Booted = 1; //True
+  int Booted = 1; // True, determines if Cobalt has booted
+  
+  // While Cobalt is booted (and has not crashed/been exited), interperet commands
   while (Booted = 1) {
+    
     char command[7];
+    int history_position = 0;
+    int history[100];
     gets(command);
-    char EXIT[] = "$EXIT";
-    if (strcmp(EXIT,command) == 0) {
-      printf(">> Ending Services...\n");
+    
+    if (sizeof(history) < 100) {
+      history[history_position] = command;
+    } else {
+      int history[100];
+    }
+
+    // Defining commands
+    char EXIT[] = ".exit";
+    char HELP[] = "help";
+    char CWD[] = "cwd";
+    char HISTORY[] = "history";
+
+    if (strcmp(EXIT, command) == 0) {
+      printf(">> Stopping services...\n");
       sleep(3);
       // Here's where all services get stopped. After this line of code.
       return 0;
-    }
-    else {
-      printf("[!] Invalid Operation [!]\n");
+
+      //Inside this else if is what the HISTORY command prints to the screen
+    } else if (strcmp(HISTORY, command) == 0) { //Doesn't work for some reason
+      for (int i = 0; i < sizeof(history)/sizeof(history[0]); i++) {
+        printf((char) history_position + ". " + i);
+      }
+
+      //Inside this else if is what the HELP command prints to the screen
+    } else if (strcmp(HELP, command) == 0) {
+      printf("\e[1;1H\e[2J");
+      printf(BLU "Cobalt " RED "HELP\n");
+      printf(RESET "\n'.exit' - Ends all Cobalt services and shuts down the OS.");
+      printf("\n'help' - View information about all available commands in the recovery environment.");
+
+      //Inside this else if is what the CWD command prints to the screen
+    } else if (strcmp(CWD, command) == 0) {
+      char cwd[PATH_MAX];
+      if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working directory: %s\n", cwd);
+
+      } else {
+        printf("getcwd() error");
+        return 1;
+      }
+
+    } else {
+      printf(YEL "[!] Invalid operation; please try again. [!]\n" RESET);
     }
   }
 }
