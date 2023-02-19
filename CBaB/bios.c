ada5c486b56ad7456c09b7d596db20d839b29c83
Erik <122167559+EriktheRDev@users.noreply.github.com>
2023-02-19 23:18:04 +0000

    Pending changes exported from your codespace

--- CBaB/bios.c
+++ CBaB/bios.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "services.h"
+#include "verbose.h"
 
 //colors for text
 #define RED "\x1B[31m"
@@ -12,38 +13,47 @@
 #define BOLD "\e[1m"
 #define RESET "\x1B[0m"
 
+
 int bios() {
-  int delay = 1; // The time to wait on the sleep() functions
+  verbose("Defined Colors.");
+  int delay = 3; // The time to wait on the sleep() functions
   printf("\e[1;1H\e[2J");             // Clears Screen.
   char os[25] = BLU "COBALT\n" RESET; // Opearting System Name, Maximum of 10 characters.
-  int ver = 4;                        // Operating System Build, Maximum of 5 characters.
-
+  verbose("Classified OS Name");
+  int ver = 8;                        // Operating System Build, Maximum of 5 characters.
+  verbose("Classified OS Data");
   printf("%s", os);        // Prints OS Name
   printf("%s", "BUILD: "); // Prints Build
   fflush(stdout);          // Flushes
   printf("%i", ver);       // Prints Version directly after Build
   fflush(stdout);          // Flushes
-
+  verbose("Completed 3 Prints and 2 Flushes.");
   sleep(delay); // Waits 3 Seconds.
-
   printf("\e[1;1H\e[2J"); // Clears Screen.
   int betabuild = -1;     // Null
+  delay = 1;
+  verbose("Betabuild has been declared as -1 [Null].");
   switch (ver) {                // Simpler Version Management
   case 100:        // If the Version is 100,
+    verbose("Betabuild has been changed. [-1 --> 0]");
     betabuild = 0; // Set Betabuild to False
     printf("%s", os);
     break;                                                                                                                                  // Exits
   default:                                                                                                                                  // If Version is NOT 100,
-    betabuild = 1;                                                                                                                          // Set Betabuild to True
-    printf("%s", os);                                                                                                                       // Prints OS
+    betabuild = 1;
+    verbose("Betabuild has been changed. [-1 --> 1]");                                                                                                                          // Set Betabuild to True
+    printf("%s", os);
+    verbose("Current Build has been marked as beta. Opening Balloon...");                                                                                                                       // Prints OS
     printf(YEL  "[!] You are currently running a beta build of Cobalt. Please upgrade to a newer version as soon as possible. [!]\n" RESET); // Prints the beta disclaimer
     break;                                                                                                                                  // Exits
   }
 
   sleep(delay);
+  verbose("Checking to see if Betabuild was changed successfully...");
   const betastatus = betabuild;
   switch (betastatus) { // Checks to see if the previous check worked properly, and checks if the variable 'BETABUILD' is 0 or 1, and not -1
-  case -1:                     // If BetaBuild is -1 (Should not be -1, if it is prints an Error.)
+  case -1:
+    verbose("Betabuild was not changed.");   // If BetaBuild is -1 (Should not be -1, if it is prints an Error.)
     printf("\e[1;1H\e[2J");    // Clears SCreen
     printf("%s", os);          // Prints OS Name
     char errortype[5] = "A.A"; // Creates the Error Type
@@ -61,7 +71,7 @@ int bios() {
   case 1:
     printf("1 [True].\n");
   }
-
+  verbose("Check completed. [Successful]");
   printf("\n>> Starting Services... ");
   sleep(delay);
   services();
--- CBaB/bootloader.c
+++ CBaB/bootloader.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "bios.h"
+#include "verbose.h"
+
+// program starts by reading the contents of the BIOS.C file
 int main() {
+  verbose("Starting File [BIOS.C]");
   bios();
 }
 
