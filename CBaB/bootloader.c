3ac9f526168996511718d80867c39573d9f13cdf
Erik <122167559+EriktheRDev@users.noreply.github.com>
2023-02-20 22:17:45 +0000

    Pending changes exported from your codespace

--- CBaB/bootloader.c
+++ CBaB/bootloader.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 #include "bios.h"
-int main() {
-  bios();
-}
+#include "verbose.h"
 
+int verboseisfalse(variable);
+int verboseistrue(variable);
+// program starts by reading the contents of the BIOS.C file
+int main() {
+  //Boot Manager
+  printf("COBALT BOOT MANAGER\n");
+  fflush(stdout);
+  sleep(1);
+  printf("1 - Start Up Normally [GUI]\n");
+  fflush(stdout);
+  printf("2 - Start Up Recovery Environment [CLI]\n");
+  fflush(stdout);
+  printf("3 - Start Up Recovery Environment [CLI + Verbose]\n");
+  fflush(stdout);
+  char response[3];
+  sleep(3);
+  gets(response);
+  char guiset[] = "1";
+  char recset[] = "2";
+  char verbst[] = "3";
+  int hasverbose;
+  if (strcmp(guiset, response) == 0) {
+    //gui code resides here
+  } else if (strcmp(recset, response) == 0) {
+    hasverbose = 0;
+    verbose("Starting File [BIOS.C]", hasverbose);
+    bios(hasverbose);
+  } else if (strcmp(verbst, response) == 0) {
+    hasverbose = 1;
+    verbose("Starting File [BIOS.C]", hasverbose);
+    bios(hasverbose);
+  } else {
+    //automatically runs gui code
+  }
+
+}
---
+++ CBaB/verbose.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "verbose.h"
+// This file contains the code to make the verbose function work
+void verbose(char message[], int variable) {
+  if (variable == 1) {
+    //char message_str[75];
+    //sprintf(message_str, "%d", message);
+    char output[100]; strcpy (output, message);
+    printf("\n{>} ");
+    printf(output);
+    printf("\n");
+  }
+}
