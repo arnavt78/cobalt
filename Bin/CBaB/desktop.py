016123ac8917db0a69f2165e96bfe3f933344ded
Norse-Horse <122167559+Norse-Horse@users.noreply.github.com>
2023-03-11 14:58:14 -0500

    Update desktop.py

--- Bin/CBaB/desktop.py
+++ Bin/CBaB/desktop.py
@@ -1,21 +1,24 @@
 # Import the necessary modules
 import os
 import PySimpleGUI as sg
+import asyncio
+import subprocess
+from launchpad import lpad
+
 # Define the menus
 menu_def = [
-    ['&File', ['&Exit']],
-    ['&Run', ['&Launchpad']],
+    ['&File', ['Exit']],
+    ['&Run', ['Launchpad']],
 ]
 guilayout = [[sg.Image('./wallpaper.png')], [sg.Menu(menu_def)]]
 
 # Create the Cobalt GUI window, with the wallpaper as the background
-window = sg.Window(title="Cobalt", layout=guilayout, margins=(-20, -20))
+window = sg.Window(title="Cobalt", layout=guilayout)
 
 while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED or event == 'Exit':
         break
     if event == 'Launchpad':
-        os.system('python ./launchpad.py') #can you go to bootloader.c plzplzplzplzplzplzplz
-
+        lpad()
 window.close()
