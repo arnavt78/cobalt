7c6b6fa897764d34e7223c13fa865e5792ab7ed5
Erik P <122167559+EriktheRDev@users.noreply.github.com>
2023-04-17 19:09:57 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,21 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let global_name: &str = "Cobalt";
-    let version: &str = "0.0.2";
-    let build: i32 = 2;
+    let version: &str = "0.0.3";
+    let build: i32 = 3;
 
     for _ in 0..25 {
         println!("\n");
     }
 
-    println!("Starting {}...\n", global_name);
+    println!("{}", global_name);
     
-    println!("Welcome to {}!", global_name);
+    println!("Version {} ({}).\n", build, version);
     println!("{} is running on build {}, v{}.\n", global_name, build, version);
 
-    println!("So far, {} has no CLI interface or a GUI.\n\nThere is no more functionality other than loading\nthe kernel and x86_64 bootloader.\n\nIf you are seeing this text, all is well so far!\n\n\nThis is all. You must do a hard shutdown as {}\nhas no safe shutting down functionality yet.\n\n\nThanks for trying out {}!", global_name, global_name, global_name);
+    println!("[There is no GUI or CLI to be found...]", global_name);
 
-    println!("\n\n\nHold down the power button to force shut down {} . . . \n\n", global_name);
+    println!("\n\n\nHold the power button to force shutdown. . . . \n\n");
 
     loop {}
 }
