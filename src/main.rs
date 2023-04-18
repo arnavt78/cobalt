021dd4e6f405e164fe4ddad038bdc996df827b22
Erik P <122167559+EriktheRDev@users.noreply.github.com>
2023-04-18 08:14:35 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,15 @@ pub extern "C" fn _start() -> ! {
 
     println!("Starting {}...\n", global_name);
     println!("{}", global_name);
-
+    use std::{thread, time};
+    thread::sleep(time::Duration::from_secs(2));
+    clearscreen::clear().expect("Error - Failure to clear screen.");
     println!("Welcome to {}!", global_name);
     println!("Version {} ({}).\n", build, version);
     println!("{} is running on build {}, v{}.\n", global_name, build, version);
 
-    println!("So far, {} has no CLI interface. There is\nno more functionality other than loading the kernel\nand x86_64 bootloader. If you are seeing this text,\nall is well so far! This is all. You must do a hard\nshutdown as {} has no safe shutting down functionality\nyet. Thanks for trying out {}!", global_name, global_name, global_name);
-    println!("\n[There is no CLI to be found...]");
+    println!("[!] So far, {} has no CLI or GUI interface. There is\nno more functionality other than loading the kernel\nand x86_64 bootloader. If you are seeing this text,\nall is well so far! This is all. You must do a hard\nshutdown as {} has no safe shutting down functionality\nyet. Thanks for trying out {}!", global_name, global_name, global_name);
+    println!("\n[There is no CLI to be found...] [!]");
     println!("> ");
 
     panic!("This is a testing panic message!");
