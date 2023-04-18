8b8381c8079543b9d7c05aae88f9a1c0841e5ea0
arnavthorat78 <arnavthorat78@gmail.com>
2023-04-18 16:40:02 +1000

    Added a test panic

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,28 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let global_name: &str = "Cobalt";
-    let version: &str = "0.0.2";
-    let build: i32 = 2;
-
-    for _ in 0..25 {
-        println!("\n");
-    }
+    let version: &str = "0.0.3";
+    let build: i32 = 3;
 
     println!("Starting {}...\n", global_name);
-    
+    println!("{}", global_name);
+
     println!("Welcome to {}!", global_name);
+    println!("Version {} ({}).\n", build, version);
     println!("{} is running on build {}, v{}.\n", global_name, build, version);
 
-    println!("So far, {} has no CLI interface or a GUI.\n\nThere is no more functionality other than loading\nthe kernel and x86_64 bootloader.\n\nIf you are seeing this text, all is well so far!\n\n\nThis is all. You must do a hard shutdown as {}\nhas no safe shutting down functionality yet.\n\n\nThanks for trying out {}!", global_name, global_name, global_name);
+    println!("So far, {} has no CLI interface. There is\nno more functionality other than loading the kernel\nand x86_64 bootloader. If you are seeing this text,\nall is well so far! This is all. You must do a hard\nshutdown as {} has no safe shutting down functionality\nyet. Thanks for trying out {}!", global_name, global_name, global_name);
+    println!("\n[There is no CLI to be found...]");
+    println!("> ");
 
-    println!("\n\n\nHold down the power button to force shut down {} . . . \n\n", global_name);
-
-    loop {}
+    panic!("This is a testing panic message!");
 }
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    println!("\nA critical error has occurred that has forced the\nOS to stop execution as a precaution. Please use\nthe information below for debugging purposes.\n\n{}", info);
+
+    println!("\nThe OS has completely stopped execution.\nTo restart, hold down the power button . . . \n");
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -9,7 +9,7 @@ lazy_static! {
     /// Used by the `print!` and `println!` macros.
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::LightGray, Color::Black),
+        color_code: ColorCode::new(Color::LightGray, Color::Blue),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
