d2a9e17118e557f1e77de8833d749f40ee184ddb
arnavthorat78 <arnavthorat78@gmail.com>
2023-04-19 11:11:56 +1000

    Changed bg color back to black

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,13 @@ pub extern "C" fn _start() -> ! {
 
     println!("Starting {}...\n", global_name);
     println!("{}", global_name);
-    //use std::{thread, time};
-    //thread::sleep(time::Duration::from_secs(2));
-    //clearscreen::clear().expect("Error - Failure to clear screen.");
+
     println!("Welcome to {}!", global_name);
     println!("Version {} ({}).\n", build, version);
     println!("{} is running on build {}, v{}.\n", global_name, build, version);
 
-    println!("[!] So far, {} has no CLI or GUI interface. There is\nno more functionality other than loading the kernel\nand x86_64 bootloader. If you are seeing this text,\nall is well so far! This is all. You must do a hard\nshutdown as {} has no safe shutting down functionality\nyet. Thanks for trying out {}!", global_name, global_name, global_name);
-    println!("\n[There is no CLI to be found...] [!]");
+    println!("the quick brown fox jumps over the lazy dog\nTHE QUICK BROWN FOX JUMPS OVER THE LAZY DOG\n0123456789\n~`!@#$%^&*()-_+=\\|{{}}[]:;\"'<,>.?/");
+    println!("\n[!] There is no CLI to be found [!]");
     println!("> ");
 
     panic!("This is a testing panic message!");
@@ -32,6 +30,6 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("\nA critical error has occurred that has forced the\nOS to stop execution as a precaution. Please use\nthe information below for debugging purposes.\n\n{}", info);
 
-    println!("\nThe OS has completely stopped execution.\nTo restart, hold down the power button . . . \n");
+    println!("\nThe OS has completely stopped execution.\nTo restart, hold down the power button . . . ");
     loop {}
 }
